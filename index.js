4397afadb9b0e09c699489bb1559d684ee527f59
Daniel Radetsky <dradetsky@gmail.com>
2019-05-31 20:33:44 -0700

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const docset = require('docset')
+
+class DocsetHandleMultiplexer {
+  constructor(docsets) {
+    this.docsets = docsets
+  }
+
+  lookup(str) {
+    return this.docsets.map(d => d.lookup(str))
+  }
+
+  merged(str) {
+    const all = this.lookup(str).reduce((acc, e) => acc.concat(e), [])
+    const sorted = all.sort((a, b) => a.name.length - b.name.length)
+    return sorted
+  }
+}
+
+function multiDocsetConvenienceFn(...dbRoots) {
+  const docsets = dbRoots.map(r => docset(r))
+  return new DocsetHandleMultiplexer(docsets)
+}
+
+Object.assign(multiDocsetConvenienceFn, {
+  DocsetHandleMultiplexer
+})
+
+module.exports = multiDocsetConvenienceFn
---
+++ readme.md
@@ -0,0 +1,19 @@
+multi-docset
+============
+
+many docsets at once!
+
+usage
+-----
+
+oh, fuck off, i can't be bothered. look at the code. jesus, you people kill me.
+
+installation
+------------
+
+git gud
+
+see also
+--------
+
+there's a docset repo. find it.
