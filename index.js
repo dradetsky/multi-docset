e8f4b5f31ab33d137772e31ee696104f73d573ab
Daniel Radetsky <dradetsky@gmail.com>
2019-06-06 10:53:10 -0700

    apply standardjs

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const docset = require('docset')
 
 class DocsetHandleMultiplexer {
-  constructor(docsets) {
+  constructor (docsets) {
     this.docsets = docsets
   }
 
-  lookup(str, opts) {
+  lookup (str, opts) {
     return this.docsets.map(d => d.lookup(str, opts))
   }
 
-  merged(str, opts) {
+  merged (str, opts) {
     const all = this.lookup(str, opts).reduce((acc, e) => acc.concat(e), [])
     const sorted = all.sort((a, b) => a.name.length - b.name.length)
     return sorted
   }
 
-  info() {
+  info () {
     return this.docsets.map(d => d.info())
   }
 }
 
-function multiDocsetConvenienceFn(files) {
+function multiDocsetConvenienceFn (files) {
   const docsets = files.map(r => docset(r))
   return new DocsetHandleMultiplexer(docsets)
 }
